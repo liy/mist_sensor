d7ac9cce220d012b13fcbcd7b70bbecbffa4e8ba
liy <liy8272@gmail.com>
2025-02-08 19:48:39 +0800

    Added real SHT40 sensor

--- main/main.c
+++ main/main.c
@@ -16,11 +16,37 @@
 #include "wireless.h"
 
 #include "comm.h"
+#include "sht4x.h"
+
+
+#define SHT4X_SDA_GPIO      GPIO_NUM_6  /*!< gpio number for I2C master data  */
+#define SHT4X_SCL_GPIO      GPIO_NUM_7  /*!< gpio number for I2C master clock */
+
+i2c_master_dev_handle_t sht4x_handle;
 
 static const char *TAG = "Mist";
 
 // Task handle to notify when slave has sent over its the address
 static TaskHandle_t s_handshake_notify = NULL;
+
+i2c_master_bus_handle_t i2c_bus_init(uint8_t sda_io, uint8_t scl_io)
+{
+    i2c_master_bus_config_t i2c_bus_config = {
+        .i2c_port = CONFIG_SHT4X_I2C_NUM,
+        .sda_io_num = sda_io,
+        .scl_io_num = scl_io,
+        .clk_source = I2C_CLK_SRC_DEFAULT,
+        .glitch_ignore_cnt = 7,
+        .flags.enable_internal_pullup = true,
+    };
+
+    i2c_master_bus_handle_t bus_handle;
+
+    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_config, &bus_handle));
+    ESP_LOGI(TAG, "I2C master bus created");
+
+    return bus_handle;
+}
 
 void nvs_init() 
 {
@@ -233,17 +259,23 @@ static esp_err_t recv_msg_cb(const CommTask_t* task)
 
 void start_sensor() 
 {
+    float temperature, humidity;
+    
     while(true) {
         ESP_LOGI(TAG, "Sending sensor data...");
+
+        esp_err_t err = sht4x_start_measurement(sht4x_handle, SHT4X_CMD_READ_MEASUREMENT_HIGH);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        err = sht4x_read_measurement(sht4x_handle, &temperature, &humidity);
 
         // Dummy sensor data
         SensorQuery query = SensorQuery_init_default;
         // Specify the the correct sensor type, so buffer size can be calculated correctly
         query.which_body = SensorQuery_mist_sensor_tag;
         query.body.mist_sensor.timestamp = time(NULL);
-        query.body.mist_sensor.humidity = (float)(rand() % 101);
-        query.body.mist_sensor.temperature = (float)(rand() % 1001) / 10.0f;
-        
+        query.body.mist_sensor.humidity = humidity;
+        query.body.mist_sensor.temperature = temperature;
+
         size_t buffer_size = 0;
         if (!pb_get_encoded_size(&buffer_size, SensorQuery_fields, &query)) {
             ESP_LOGE(TAG, "Failed to get encoded size");
@@ -301,9 +333,27 @@ void app_main(void)
     while(!ulTaskNotifyTake(pdTRUE, 1000 / portTICK_PERIOD_MS)) {
         ESP_LOGI(TAG, "Waiting for handshake...");
     }
-
     ESP_LOGI(TAG, "Handshake done");
-    xTaskCreate(start_sensor, "start_sensor", 4096, NULL, 5, NULL);
+
+
+    // Initialize I2C bus
+    i2c_master_bus_handle_t bus_handle = i2c_bus_init(SHT4X_SDA_GPIO, SHT4X_SCL_GPIO);
+    sht4x_handle = sht4x_device_create(bus_handle, SHT4X_I2C_ADDR_0, CONFIG_SHT4X_I2C_CLK_SPEED_HZ);
+    ESP_LOGI(TAG, "Sensor initialization success");
+
+
+    // Probe the sensor to check if it is connected to the bus with a 10ms timeout
+    esp_err_t err = i2c_master_probe(bus_handle, SHT4X_I2C_ADDR_0, 200);
+
+    if(err == ESP_OK) {
+        ESP_LOGI(TAG, "SHT4X sensor found");
+        xTaskCreate(start_sensor, "start_sensor", 4096, NULL, 5, NULL);
+    } else {
+        ESP_LOGE(TAG, "SHT4X sensor not found");
+        sht4x_device_delete(sht4x_handle);
+    }
+
+
 
     // Dummy main loop
     while (1) {
